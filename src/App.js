66e35310a7ddd3e7b83347c776be09020b6bbef5
liyi <584531510@qq.com>
2022-11-14 16:28:48 +0800

    feat: 轮播图

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from 'react'
-import Test from './components/common/RadioBoxGroup/Test'
-import { A, B } from './components/common/Comp'
-import withLog from './components/common/HOC/withLog'
-
-const ALog = withLog(A)
-const BLog = withLog(B)
+import Test from './components/common/Banner/Test'
 
 export default function App() {
     return (
